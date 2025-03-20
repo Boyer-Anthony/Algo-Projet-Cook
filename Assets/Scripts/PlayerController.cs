5e24fefd36ac02afb8626aaa3567b6edd52ca40d
Boyer-Anthony <a.boyer@rubika-edu.re>
2025-03-20 21:23:23 +0400

    Ajout d'un Curseur et d'une Pile

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -7,8 +8,12 @@ public class PlayerController : MonoBehaviour
 {
     public Camera cam;
     public NavMeshAgent agent;
+    public GameObject Cursor;
 
-    
+    private Vector3 targetCursor;  
+    private Stack<GameObject> pile = new Stack<GameObject>();
+
+
     // Update is called once per frame
     void Update()
     {
@@ -16,12 +21,30 @@ public class PlayerController : MonoBehaviour
         {
             Ray ray = cam.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
+           
+            
 
             if (Physics.Raycast(ray, out hit))
             {
-                agent.SetDestination(hit.point);
 
-                // animation
+                
+                targetCursor = hit.point;            // Recuperer la position du Cursor
+                agent.SetDestination(hit.point);    // Focus la position du Cursor
+
+                // Supprime le dernier curseur s'il existe
+                if (pile.Count > 0)
+                {
+                    GameObject lastCursor = pile.Pop();
+                    Destroy(lastCursor);
+                   
+                }
+
+                // Instanciation du Cursor à la position du hit
+                GameObject newCursor = Instantiate(Cursor, targetCursor, Quaternion.identity);
+
+                // Ajouter élément dans la stack
+                pile.Push(newCursor);
+
             }
         }
 
